10ddc58ff0e59f772b2b89b925d24478b8d5973f
kashifanoorre-code <kashifanoorre@gmail.com>
2025-08-23 23:11:27 +0530

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main() {
+    float radius, height, volume, volumeofcylinder;
+    printf("enter radius and height: ");
+    scanf("%f %f", &radius , &height);
+    volumeofcylinder = PI*radius*radius*height;
+    printf("\nthe volume of cylinder is: %f ", volumeofcylinder);
+    
+
+    return 0;
+}
